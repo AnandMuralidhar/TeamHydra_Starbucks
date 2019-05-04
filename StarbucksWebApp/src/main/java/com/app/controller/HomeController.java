ae3c7d191a61943dda9c8e60d40faf05ec403b8f
ChaiDeshmukh <chaitrali.deshmukh7@gmail.com>
2019-05-04 16:12:52 -0700

    adding database setup

--- StarbucksWebApp/src/main/java/com/app/controller/HomeController.java
+++ StarbucksWebApp/src/main/java/com/app/controller/HomeController.java
@@ -1,14 +1,65 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.app.model.User;
+import com.app.service.UserService;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/")
 	public String start() {
 		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+	
+	
+	@GetMapping("/success")
+	public String success() {
+		return "success";
+	}
+	
+
+	
+	@PostMapping("/addUser")
+	public String addUser(@RequestParam("email") String email, @RequestParam("fname") String fname, @RequestParam("lname") String lname,
+			@RequestParam("zipcode") int zipcode, @RequestParam("password") String password)
+	{
+		User user = new User();
+	/*	user.setEmailID(email);
+		user.setFirstName(fname);
+		user.setLastName(lname);
+		user.setZipCode(zipcode);
+		user.setPassword(password);*/
+		
+		user.setEmailID("abc@gmail.com");
+		user.setFirstName("ABC");
+		user.setLastName("XYZ");
+		user.setZipCode(98);
+		user.setPassword("root");
+		
+		
+		if(userService.adduser(user))
+		{
+			System.out.println("User added successfully");
+		}else
+		{
+			System.out.println("User not added successfully");
+		}
+		
+	return "success";
 	}
 
 }
